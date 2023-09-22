6e2063fbd100d281a7cc28746d31a22379dafcca
fluffymariam <adefowobiladejobi@gmail.com>
2023-09-21 17:56:01 -0700

    l

--- opcode_functions.c
+++ opcode_functions.c
@@ -31,3 +31,19 @@ void execute_opcode(char *opcode, char *opcode_argument, unsigned int line_numbe
 			exit(EXIT_FAILURE);
 		}
 }
+
+/**
+ * pint - Print the value at the top of the stack.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: Line number being executed.
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
